c180ee448e54f4b85767fa86a1fa118bafaee85c
Aayushmaan Singh Meyan <aayushmaansinghmeyan@gmail.com>
2026-02-06 13:37:10 +0530

    Add files via upload

---
+++ Divide and Conquer/Lab Questions/Question1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int target) {
+    int left = 0, right = n - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1; 
+    }
+    return -1; 
+}
+
+int main() {
+    int arr[] = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int target = 23;
+
+    int index = binarySearch(arr, n, target);
+
+    if (index != -1)
+        cout << "Target found at index: " << index;
+    else
+        cout << "Target not found";
+
+    return 0;
+}
---
+++ Divide and Conquer/Lab Questions/Question4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int maxSubarraySum(int arr[], int n) {
+    int maxSoFar = arr[0];
+    int currentSum = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        currentSum = max(arr[i], currentSum + arr[i]);
+        maxSoFar = max(maxSoFar, currentSum);
+    }
+
+    return maxSoFar;
+}
+
+int main() {
+    int arr[] = {-2, -5, 6, -2, -3, 1, 5, -6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    cout << "Maximum subarray sum = " << maxSubarraySum(arr, n);
+
+    return 0;
+}
